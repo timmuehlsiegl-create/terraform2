7dbcb33ffa1ee462ffb970dade1aa6c5c8e3d1c2
Dim <tim.muehlsiegl@gmail.com>
2026-03-10 20:00:00 +0100

    test commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+   backend "s3" {
+    bucket = "REPLACE-WITH-YOUR-BUCKET-NAME"
+    key    = "demo/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "demo" {
+  ami           = "ami-0b6c6ebed2801a5cb" # ubuntu
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test-server-iac"
+  }
+}
+output "instance_id" {
+  value = aws_instance.demo.id
+}
+output "public_ip" {
+  value = aws_instance.demo.public_ip
+}
---
+++ terraform_getting_started/readme.md
@@ -0,0 +1,18 @@
+# Terraform Getting Started
+
+
+## Umgebungsvariabken setzten
+
+
+### Windows Env-Variablen setzen
+
+Linux
+export AWS_ACCESS_KEY_ID=
+
+export AWS_SECRET_ACCESS_KEY=
+
+export AWS_DEFAULT_REGION=us-east-1
+
+Windows
+$env:AWS_ACCESS_KEY_ID="" 
+$env:AWS_SECRET_ACCESS_KEY="" $env:AWS_DEFAULT_REGION="us-east-1"
